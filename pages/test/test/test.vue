87552f233dbe0d69f48d60969d4b5a1da1287207
MRGUOKING <420919469@qq.com>
2023-04-01 22:32:47 +0800

    初始化移动端代码

---
+++ pages/test/test/test.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<h1>测试页面</h1>
+		<h2>测试页面11111111</h2>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
